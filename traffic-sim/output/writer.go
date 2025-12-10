50f796467f2cb482a9eec4d554c11a6f55ee249d
Aidan Heinle <heinleaidan@gmail.com>
2025-12-10 00:25:55 -0600

    Add WriteCSV function for CSV output
    Implement WriteCSV function to export simulation data to a CSV file.

--- traffic-sim/output/writer.go
+++ traffic-sim/output/writer.go
@@ -1,1 +1,46 @@
+package output
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+
+	"traffic-sim/sim"
+)
+
+func WriteCSV(filename string, Data [][]sim.TickData) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{
+		"Tick",
+		"LaneCount",
+		"CarCount",
+		"AverageSpeed",
+		"Density",
+		"Capacity",
+	})
+
+	for _, tickData := range Data {
+		for _, row := range tickData {
+			writer.Write([]string{
+				strconv.Itoa(row.TickNumber),
+				strconv.Itoa(row.LaneCount),
+				strconv.Itoa(row.CarCount),
+				strconv.FormatFloat(row.AverageSpeed, 'f', 4, 64),
+				strconv.FormatFloat(row.Density, 'f', 4, 64),
+				strconv.FormatFloat(row.Capacity, 'f', 4, 64),
+			})
+		}
+	}
+
+	return nil
+}
 
