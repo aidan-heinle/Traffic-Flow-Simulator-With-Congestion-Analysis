46f2caf695b68bf6e32bc061039086c72284fef7
Aidan Heinle <heinleaidan@gmail.com>
2025-12-10 00:26:30 -0600

    Define RoadSegment, TickData, and World structures
    Added data structures for road segments and simulation.

--- traffic-sim/sim/model.go
+++ traffic-sim/sim/model.go
@@ -1,10 +1,27 @@
 package sim
 
+// represents a single piece of roadway
 type RoadSegment struct {
+	SegmentID    int
+	Length       float64 // meters
+	LaneCount    int
+	CarCount     int
+	AverageSpeed float64 // meters per second
 }
 
+// telementry data for CSV
 type TickData struct {
+	TickNumber   int
+	LaneCount    int
+	CarCount     int
+	AverageSpeed float64
+	Density      float64
+	Capacity     float64
 }
 
+// simulation settings + container
 type World struct {
+	SegmentList []RoadSegment
+	TickCount   int
+	// settings
 }
